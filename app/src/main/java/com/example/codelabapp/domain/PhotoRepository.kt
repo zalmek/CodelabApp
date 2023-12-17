83cb10de3864569f5151dbfacfaa276bda8077cc
Zalmek23 <zalmek23@gmail.com>
2023-12-17 19:35:18 +0300

    Readme fix

--- app/src/main/java/com/example/codelabapp/domain/PhotoRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.codelabapp.domain
-
-import com.example.codelabapp.model.Photo
-import kotlinx.coroutines.flow.Flow
-
-interface PhotoRepository {
-    fun getAllPhotos(): Flow<List<Photo>>
-}
--- app/src/main/java/com/example/codelabapp/network/ApiHelper.kt
+++
@@ -1,9 +0,0 @@
-package com.example.codelabapp.network
-
-import com.example.codelabapp.model.Photo
-
-class ApiHelper(private val codelabApi: CodelabApi) {
-    suspend fun getAllPhotos(): List<Photo> {
-        return codelabApi.getAllPhotos()
-    }
-}
